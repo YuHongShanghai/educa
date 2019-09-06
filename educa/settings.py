cee644f0ac64a83be880646d25930e5e3c7ff258
yuhong <yh951012@163.com>
2019-09-06 18:01:25 +0800

    0906

--- educa/settings.py
+++ educa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'embed_video',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,9 @@ ROOT_URLCONF = 'educa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'courses/templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'courses/templates'),
+                 os.path.join(BASE_DIR, 'students/templates')
+                 ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL=reverse_lazy('student_course_list')
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
+
--- students/views.py
+++ students/views.py
@@ -9,6 +9,7 @@ from django.views.generic.detail import DetailView
 from courses.models import Course
 from .forms import CourseEnrollForm
 
+
 class StudentRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
 
@@ -36,4 +37,28 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
         return reverse_lazy('student_course_detail',
                             args=[self.course.id])
 
+class StudentCourseListView(LoginRequiredMixin,ListView):
+    model=Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs=super(StudentCourseListView,self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+class StudentCourseDetailView(DetailView):
+    model=Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs=super(StudentCourseDetailView,self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+    def get_context_data(self,**kwargs):
+        context=super(StudentCourseDetailView,self).get_context_data(**kwargs)
+        course=self.get_object()
+        if 'module_id' in self.kwargs:
+            context['module']=course.modules.get(id=self.kwargs['module_id'])
+        else:
+            context['module']=course.modules.all()[0]
+        return context
 
